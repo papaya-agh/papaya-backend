c5dc0f5598078696a9470327fabe99751e7c5992
Szymon Bernal <szymon.bernal@gmail.com>
2019-04-03 16:53:26 +0200

    IO2019TEAM-28 Add User and UserInProject entities

---
+++ data/src/main/java/pl/edu/agh/model/BaseEntity.java
@@ -0,0 +1,21 @@
+package pl.edu.agh.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
--- data/src/main/java/pl/edu/agh/model/UserInProject.java
+++ data/src/main/java/pl/edu/agh/model/UserInProject.java
@@ -1,5 +1,30 @@
 package pl.edu.agh.model;
 
-public class UserInProject {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class UserInProject extends BaseEntity {
+    // TODO remove after Project entity is created
+    @Transient
+    private Project project;
+
+    @ManyToOne
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 8)
+    private UserRole userRole;
 }
