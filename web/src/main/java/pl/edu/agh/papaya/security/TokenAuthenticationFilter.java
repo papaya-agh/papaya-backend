f64e7ba845222a5af9822418f74364621da5d2d6
Kamil Jarosz <kamil09875@gmail.com>
2019-04-14 18:28:40 +0200

    IO2019TEAM-42 Allow h2 web console

--- web/src/main/java/pl/edu/agh/papaya/security/TokenAuthenticationFilter.java
+++ web/src/main/java/pl/edu/agh/papaya/security/TokenAuthenticationFilter.java
@@ -1,8 +1,13 @@
 package pl.edu.agh.papaya.security;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.FilterChain;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,8 +26,15 @@ public class TokenAuthenticationFilter extends AbstractAuthenticationProcessingF
 
     private static final String BEARER_PREFIX = "Bearer ";
 
+    private static final List<Pattern> UNSECURED_URLS = Arrays.asList(
+            Pattern.compile("/login/?"),
+            Pattern.compile("/h2(|/.*)"));
+
     @Autowired
     private TokenRepository tokenRepository;
+
+    @Autowired
+    private ServletContext servletContext;
 
     public TokenAuthenticationFilter() {
         super(req -> true);
@@ -34,8 +46,15 @@ public class TokenAuthenticationFilter extends AbstractAuthenticationProcessingF
 
     @Override
     protected boolean requiresAuthentication(HttpServletRequest request, HttpServletResponse response) {
-        String requestURI = request.getRequestURI();
-        return !requestURI.startsWith("/rest/login");
+        String contextPath = servletContext.getContextPath();
+        String requestUri = request.getRequestURI();
+        String relativeRequestUri = requestUri.startsWith(contextPath) ?
+                requestUri.substring(contextPath.length()) :
+                requestUri;
+
+        return UNSECURED_URLS.stream()
+                .map(p -> p.matcher(relativeRequestUri))
+                .noneMatch(Matcher::matches);
     }
 
     @Override
