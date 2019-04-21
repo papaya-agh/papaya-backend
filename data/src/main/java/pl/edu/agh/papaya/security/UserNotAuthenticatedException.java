f7fbe80d6c365db1e8d29db069d465c854bf015d
Kamil Jarosz <kamil09875@gmail.com>
2019-04-21 23:34:34 +0200

    IO2019TEAM-53 Disable checks for integration tests

--- data/src/main/java/pl/edu/agh/papaya/security/UserNotAuthenticatedException.java
+++ data/src/main/java/pl/edu/agh/papaya/security/UserNotAuthenticatedException.java
@@ -3,7 +3,7 @@ package pl.edu.agh.papaya.security;
 public class UserNotAuthenticatedException extends RuntimeException {
 
     public UserNotAuthenticatedException() {
-
+        super();
     }
 
     public UserNotAuthenticatedException(String message) {
