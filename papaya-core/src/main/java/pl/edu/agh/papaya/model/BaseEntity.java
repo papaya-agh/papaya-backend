24124b4c4de237c078500a75b719b6073cf94a90
Kamil Jarosz <kamil09875@gmail.com>
2019-04-27 18:52:48 +0200

    IO2019TEAM-55 Add equals and hashCode to BaseEntity

--- papaya-core/src/main/java/pl/edu/agh/papaya/model/BaseEntity.java
+++ papaya-core/src/main/java/pl/edu/agh/papaya/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package pl.edu.agh.papaya.model;
 
+import java.util.Objects;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -19,4 +20,23 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(id);
+    }
 }
